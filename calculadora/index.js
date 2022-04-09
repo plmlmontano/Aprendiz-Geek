43b36c3a26006cb280baccbca5c87c55d2cbc4d2
plmlmontano <101532494+plmlmontano@users.noreply.github.com>
2022-04-09 16:39:01 -0500

    refactorizacion de codigo #4

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,4 +1,4 @@
-const btnCalcular = document.getElementById( 'btn-calcular' );
+/* const btnCalcular = document.getElementById( 'btn-calcular' );
 
 btnCalcular.addEventListener( 'click', function(){
   let cuenta = parseInt( document.querySelector( '#cuenta' ).value );
@@ -16,4 +16,41 @@ btnCalcular.addEventListener( 'click', function(){
   totalCuenta.innerHTML = cuenta;
   totalPropina.innerHTML = calculoTotalPropina;
   totalPagar.innerHTML = calculoTotalPagar;
-} );
+} ); */
+
+// Refactorizacion del codigo inicial
+
+const btnCalcular = document.getElementById("btn-calcular");
+const cuenta = document.getElementById("cuenta");
+const propina = document.getElementById("propina");
+
+const totalCuenta = document.getElementById("total-cuenta");
+const totalPropina = document.getElementById("total-propina");
+const totalPagar = document.getElementById("total-pagar");
+
+const calcular = (cuenta, propina) => {
+
+  //Calculamos el total de la propina
+  let calculoTotalPropina = (cuenta * propina) / 100;
+  //Calculamos el total de la cuenta
+  let calculoTotalPagar = cuenta + calculoTotalPropina;
+
+
+  //Mostramos la información en pantalla
+  totalCuenta.innerHTML = cuenta;
+  totalPropina.innerHTML = calculoTotalPropina;
+  totalPagar.innerHTML = calculoTotalPagar;
+  
+}
+
+btnCalcular.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (isNaN(parseInt(cuenta.value)) || isNaN(parseInt(propina.value))) {
+    alert('Ingresa los datos solicitados')
+  } else {
+    calcular(parseInt(cuenta.value), parseInt(propina.value))
+    cuenta.value = ""
+    propina.value = ""
+  }
+
+})
